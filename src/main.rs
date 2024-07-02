f9d4ec7998743039cc93d5057b9f05aa2f65bbf8
Niklas Hempel <niklashempel9@gmail.com>
2024-07-02 16:29:27 +0200

    vga text mode

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+mod vga_buffer;
+
+use core::{fmt::write, panic::PanicInfo};
 
 static HELLO: &[u8] = b"Hello World!";
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(
+        vga_buffer::WRITER.lock(),
+        ", some numbers: {} {}",
+        42,
+        1.337
+    )
+    .unwrap();
+    println!("\nHello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
